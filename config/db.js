1de47bee39136eb71921e48d8db73f4780f88716
SamadhanBhutekar <59012222+SamadhanBhutekar@users.noreply.github.com>
2025-02-20 23:47:14 +0530

    Update db.js
    db

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb+srv://admin:admin%40123@admin.fvwcg.mongodb.net/Hospital_Management?retryWrites=true&w=majority&appName=admin')
-  .then(() => console.log('MongoDB connected successfully'))
-  .catch(err => console.error('MongoDB connection error:', err));
-
+  mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((error) => console.error("MongoDB Connection Error:", error));
 
