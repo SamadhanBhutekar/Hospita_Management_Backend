4365bac20d17aa6de7a4cebca6dcdb8583bde845
SamadhanBhutekar <samadhanbhutekar02@gmail.com>
2025-02-20 23:10:19 +0530

    add cors details

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ app.use(cors());
 app.use(express.json());
 const PORT = 4000;
 
+app.use(cors({
+  origin: "*", 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true 
+}))
+
 // ------------------------Registration Api-----------------------------
 
 // ---------------------------validation Api-------------------------
